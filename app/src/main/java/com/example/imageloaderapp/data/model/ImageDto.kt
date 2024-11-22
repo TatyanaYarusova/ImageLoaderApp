f3f2c1351798fec209ac33293745ef00c7d1a0b3
Tatyana Yarusova <tata_yarusova@mail.ru>
2024-11-22 18:53:43 +0300

    add request network

---
+++ app/src/main/java/com/example/imageloaderapp/data/model/ImageDto.kt
@@ -0,0 +1,12 @@
+package com.example.imageloaderapp.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ImageDto(
+
+    @SerializedName("id")
+    val id: Int,
+
+    @SerializedName("url")
+    val url: String
+)
