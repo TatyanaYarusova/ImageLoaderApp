84e1e993eeabf91a05cee5e20571f86aaba9743c
Tatyana Yarusova <tata_yarusova@mail.ru>
2024-11-22 20:49:40 +0300

    add adapter and view model in fragment

--- app/src/main/java/com/example/imageloaderapp/presentation/ui/ImageListFragment.kt
+++ app/src/main/java/com/example/imageloaderapp/presentation/ui/ImageListFragment.kt
@@ -1,25 +1,59 @@
 package com.example.imageloaderapp.presentation.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.imageloaderapp.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.imageloaderapp.databinding.FragmentImageListBinding
+import com.example.imageloaderapp.domain.entity.Image
+import com.example.imageloaderapp.presentation.state.ScreenState
+import com.example.imageloaderapp.presentation.ui.adapter.ImageAdapter
+import com.example.imageloaderapp.presentation.viewmodel.ImageListViewModel
 
 class ImageListFragment : Fragment() {
 
+    private var _binding: FragmentImageListBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val adapter = ImageAdapter()
+
+    private val viewModel by lazy {
+        ViewModelProvider(this)[ImageListViewModel::class.java]
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image_list, container, false)
+    ): View {
+        _binding = FragmentImageListBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.state.observe(viewLifecycleOwner, ::renderState)
+    }
+
+    private fun renderState(state: ScreenState<List<Image>>) {
+        when (state) {
+            is ScreenState.Content -> renderContent(state.content)
+            is ScreenState.Error -> {}
+            is ScreenState.Loading -> {}
+
+        }
+    }
+
+    private fun renderContent(content: List<Image>) {
+        Log.d("TAG", content.toString())
+        binding.rv.adapter = adapter
+        adapter.submitList(content)
     }
 
     companion object {
